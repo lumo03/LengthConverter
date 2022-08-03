c497ffcda7240a83f67eb256356f281e64610dd0
Luis Moncada <luis.moncada.mso@gmail.com>
2022-08-03 14:51:13 +0200

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Luis Moncada on 03.08.22.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
